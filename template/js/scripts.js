ec6c22fd508056862a4dfa0f53a18f82a4605c7a
Sheil <karaulov.z@mail.ru>
2024-07-04 22:14:32 +0300

    Десктоп готов

--- template/js/scripts.js
+++ template/js/scripts.js
@@ -1,3 +1,17 @@
-document.querySelector(".frame-download__select").addEventListener('click', () => {
-  document.querySelector(".frame-download__list").classList.add('open');
+document.addEventListener("DOMContentLoaded", () =>{
+  // Выпадающее меню
+  document.querySelector(".frame-download__select").addEventListener('click', () => {
+    document.querySelector(".frame-download__list").classList.toggle('open');
+  });
+  document.querySelector(".frame-download__button").addEventListener('click', () => {
+    document.querySelector(".frame-download__list").classList.remove('open');
+  });
+
+  // Вращение загрузки
+  document.querySelector(".frame__limit .button").addEventListener('click', () => {
+    document.querySelector(".frame-limit__load").classList.add('loading');
+    setTimeout(() => {
+      document.querySelector(".frame-limit__load").classList.remove('loading');
+    }, 2500);
+  });
 });
